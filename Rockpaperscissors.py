41d96bc190e6b2c4ba2dd0260e23f7bcaa1ed83c
Shuna <99847731+Voidsowo@users.noreply.github.com>
2023-06-26 18:00:54 -0600

    Rock Paper Scissors Minigame

---
+++ Rockpaperscissors.py
@@ -0,0 +1,33 @@
+from random import randint
+
+list = ["Rock", "Paper", "Scissors"]
+
+ai = list[randint(0,2)]
+
+player = False
+while player == False:
+
+    print ("Rock, Paper, Scissors ")
+    player = input("Enter your move: " )
+
+    if player == ai:
+        print("Tie")
+    elif player == "Rock":
+        if ai == "Paper":
+            print("You lose, The computer played " + ai)
+        else: print("You win, The computer played " + ai)
+    elif player == "Scissors":
+        if ai == "Paper":
+            print("You win, The computer played " + ai)
+        else: print("You lose, The computer played " + ai)
+    elif player.lower() == "exit":
+        print("Thank you for playing")
+        print()
+        break
+    else:
+        print("That's not a valid play. Check your spelling")
+
+    print()
+
+    player = False
+    ai = list[randint(0,2)]
